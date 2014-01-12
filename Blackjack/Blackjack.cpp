e21aefc99674b8a600699cfad0331ad07dce11bd
Richard Hancock <richard@spacecrazyproductions.com>
2014-01-12 05:19:24 +0000

    Removed Globals and added Char Arrays

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -1,65 +1,27 @@
 #include <iostream>
 #include <time.h>
 #include <array>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-struct Card
-{
-	string name; 
-	short value; // 0 for ace due to it being 1 or 11
-	string suit;
-};
-
-struct Player
-{
-	int bet;
-	int balance;
-	int pIndex;
-	int handTotal;
-	Card hand[12];
-};
-
-struct Dealer
-{
-	int dIndex;
-	int handTotal;
-	Card hand[12];
-};
-
-//Function Prototypes
-void shuffleDeck(void);
-void drawCards(int,char);
-int randomNumber(int,int);
-void resetDeck(void);
-string suitSwitch(string);
-void checkForBlackjack(short);
-void mainGameLoop();
-short playersTurn();
-short dealersTurn();
-int handTotal(Card,short);
-bool isBustCheck(int);
-void resetHands(void);
-void menuValidation(void);
-void actionValidation(void);
-
 //Card Values
-const short ACE = 0;
+const short ACE = 1;
 const short FACECARD = 10;
 //Special Characters
 const char HEARTS = '\x03';
 const char DIAMONDS = '\x04';
 const char CLUBS = '\x05';
 const char SPADES = '\x06';
+const char CHAR_NULL = '\0';
 
-const string CARDNAMES[13] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+//CHANGEconst string CARDNAMES[13] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 const short NUM_OF_SUITS = 4;
 const short NUM_OF_CARDS_PER_SUIT = 13;
 const short NUM_OF_CARDS_IN_DECK = 52;
-
+const short MAX_NUM_OF_CARDS_IN_HAND = 12; //11 is the max, but 12th is the card that will cause bust
 //Player Actions
-const short HIT = 1; 
+const short HIT = 1;
 const short STAND = 2;
 
 //Identifiers
@@ -71,17 +33,88 @@ const short NORMAL = 1;
 const short BLACKJACK = 2;
 const short BUST = 3;
 
-//Destroy all these start
-Card g_deck[NUM_OF_CARDS_IN_DECK]; // Create a global int array to contain the deck
-short g_positionInDeck;
+struct Card
+{
+	char name[6]; 
+	short value; // 0 for ace due to it being 1 or 11
+	char suit;
+};
 
-Card g_playersHand[11];
-short g_playersCardCount;
-Card g_dealersHand[10];
-short g_dealersCardCount;
+struct Deck
+{
+	short posInDeck; //Current position in the deck
+	Card cards[52];
+};
 
-int g_playerMoney = 1000;
-//Destroy all these end
+struct Player
+{
+	int bet;
+	int balance;
+	int handTotal;
+	int numOfCards;
+	Card hand[MAX_NUM_OF_CARDS_IN_HAND]; // Maximum possible cards
+};
+
+struct Dealer
+{
+	int handTotal;
+	int numOfCards;
+	Card hand[MAX_NUM_OF_CARDS_IN_HAND];
+};
+
+//Function Prototypes
+void shuffleDeck(Deck &);
+void drawCards(int,char, Deck &, Player &, Dealer &);
+int randomNumber(int,int);
+void resetDeck(Deck &);
+char suitSwitch(char);
+void checkForBlackjack(short);
+void mainGameLoop(Deck &, Player &, Dealer &);
+short playersTurn(Deck &, Player &, Dealer &);
+short dealersTurn(Deck &, Player &, Dealer &);
+int handTotal(Card,short);
+bool isBustCheck(int);
+void resetHands(Player &, Dealer &);
+void menuValidation(void);
+void actionValidation(void);
+void resetGame(Player &);
+
+
+
+int main(void) 
+{
+	Deck deck;
+
+	Player player;
+	Dealer dealer;
+
+	resetGame(player);
+	resetDeck(deck);
+	resetHands(player, dealer);
+	shuffleDeck(deck);
+	mainGameLoop(deck, player, dealer);
+
+	//cout << deck.cards[1].name << endl << deck.cards[1].value << endl << deck.cards[1].suit << endl ;
+	
+	/* debug loop
+	for (int i = 0; i < 52; i++)
+	{
+		cout << i << ", " << deck.cards[i].name << ", " << deck.cards[i].value << ", " << deck.cards[i].suit << endl;
+	}
+	
+	debug hand display
+	drawCards(2,PLAYER);
+	drawCards(2,DEALER);
+	cout << "Player Hand: "<< g_playersHand[0].name << ", " << g_playersHand[0].value << ", " <<  g_playersHand[0].suit << ";\n" << g_playersHand[1].name << ", " << g_playersHand[1].value << ", " << g_playersHand[1].suit << ";" << endl;
+	cout << "Dealer Hand: "<< g_dealersHand[0].name << ", " << g_dealersHand[0].value << ", " <<  g_dealersHand[0].suit << ";\n" << g_dealersHand[1].name << ", " << g_dealersHand[1].value << ", " << g_dealersHand[1].suit << ";" << endl;
+	*/
+	//Menu loop
+	//Cin plus validation
+	//Switch statement
+	//End Menu loop
+	
+	return 0;
+}
 
 int randomNumber(int minimum, int maximum)
 {
@@ -101,117 +134,146 @@ int handTotal(Card hand[], short cardCount)
 	return total;
 }
 
-string suitSwitch(string suit)
+char suitSwitch(char suit)
 {
-	
-	if (suit == "Hearts")
-	{
-		return "Diamonds";
-	}
+	switch (suit) {
+		case HEARTS:
+			return DIAMONDS;
+		break;
 
-	if (suit == "Diamonds")
-	{
-		return "Clubs";
-	}
-	
-	if (suit == "Clubs")
-	{
-		return "Spades";
-	}
-	
-	return "Done";
-}
-
-void resetHands(void)
-{
-	for (int i = 0; i < 11; i++)
-	{
-		g_playersHand[i].name = "";
-		g_playersHand[i].value = -1;
-		g_playersHand[i].suit = "";
+		case DIAMONDS:
+			return CLUBS;
+		break;
 		
-		if (i < 10)
-		{
-			g_dealersHand[i].name = "";
-			g_dealersHand[i].value = -1;
-			g_dealersHand[i].suit = "";
-		}
+		case CLUBS:
+			return SPADES;
+		break;
+
+		default:
+			return CHAR_NULL;
+		break;
 	}
-	g_playersCardCount = 0;
-	g_dealersCardCount = 0;
 }
 
-void resetDeck(void)
+void resetGame(Player &player) 
 {
 	// Seed random number generator
 	srand(time(NULL));
 
-	// Reset Deck
-	string currentSuit = "Hearts";
-	g_positionInDeck = 0;
+	// Reset players balance
+	player.balance = 1000;
+}
 
-	for (int j = 1; j <= NUM_OF_SUITS; j++)
+void resetDeck(Deck &deck)
+{		
+	char currentSuit = HEARTS;
+	deck.posInDeck = 0;
+
+	for (int suitIterator = 1; suitIterator <= NUM_OF_SUITS; suitIterator++) // Loop through every suit
 	{
-		for (int k = 1; k <= NUM_OF_CARDS_PER_SUIT; k++)
+
+		for (int cardIterator = 1; cardIterator <= NUM_OF_CARDS_PER_SUIT; cardIterator++) //Loop through every card in a suit
 		{
-			
-			if (k >= 2 && k <= 10)
+
+			if (cardIterator >= 2 && cardIterator <= 9)
 			{
-				g_deck[g_positionInDeck].name = CARDNAMES[k-1];
-				g_deck[g_positionInDeck].value = k;
-				g_deck[g_positionInDeck].suit = currentSuit;
+				deck.cards[deck.posInDeck].name[0] = (48 + cardIterator); //ASCII position of 0 plus current number
+				deck.cards[deck.posInDeck].name[1] = CHAR_NULL; //Correctly ending CharArray
+				deck.cards[deck.posInDeck].value = cardIterator;
+				deck.cards[deck.posInDeck].suit = currentSuit;
 			}
 			else
 			{
 
-				if (k >= 11 && k <= NUM_OF_CARDS_PER_SUIT) 
+				if (cardIterator >= 11 && cardIterator <= NUM_OF_CARDS_PER_SUIT) 
 				{
-					g_deck[g_positionInDeck].name = CARDNAMES[k-1];
-					g_deck[g_positionInDeck].value = 10;
-					g_deck[g_positionInDeck].suit = currentSuit;
+					deck.cards[deck.posInDeck].value = 10;
+					deck.cards[deck.posInDeck].suit = currentSuit;
+					switch (cardIterator) {
+						case 11:
+							strcpy(&deck.cards[deck.posInDeck].name[0],"Jack");
+						break;
+						case 12:
+							strcpy(&deck.cards[deck.posInDeck].name[0],"Queen");
+						break;
+						case 13:
+							strcpy(&deck.cards[deck.posInDeck].name[0],"King");
+						break;
+					}
 				}
 				else
 				{
-					if (k == 1) 
+
+					if (cardIterator == 1) 
 					{
-						g_deck[g_positionInDeck].name = CARDNAMES[0];
-						g_deck[g_positionInDeck].value = ACE;
-						g_deck[g_positionInDeck].suit = currentSuit;
+						strcpy(&deck.cards[deck.posInDeck].name[0],"Ace");
+						deck.cards[deck.posInDeck].value = ACE;
+						deck.cards[deck.posInDeck].suit = currentSuit;
+					}
+					else //10 card
+					{
+						strcpy(&deck.cards[deck.posInDeck].name[0],"10");
+						deck.cards[deck.posInDeck].value = 10;
+						deck.cards[deck.posInDeck].suit = currentSuit;
 					}
 				}
 
 			}
-			
-			g_positionInDeck++;
+
+			//Switch to next card in deck
+			deck.posInDeck++;
 		}
+
+		// Switch to next suit
 		currentSuit = suitSwitch(currentSuit);
 	}
 
-	g_positionInDeck = 0;
+	//Set position back to the top of the deck
+	deck.posInDeck = 0;
 	
 }
 
-void shuffleDeck(void)
+void resetHands(Player &player, Dealer &dealer)
 {
-	Card temp;
+	for (int i = 0; i < MAX_NUM_OF_CARDS_IN_HAND; i++)
+	{
+		player.hand[i].name[0] = CHAR_NULL;
+		player.hand[i].value = -1;
+		player.hand[i].suit = CHAR_NULL;
+		
+		dealer.hand[i].name[0] = CHAR_NULL;
+		dealer.hand[i].value = -1;
+		dealer.hand[i].suit = CHAR_NULL;
+	}
+	player.numOfCards = 0;
+	dealer.numOfCards = 0;
+
+	player.bet;
+}
+
+void shuffleDeck(Deck &deck)
+{
+
 	int index1 = 0, index2 = 0;
 
 	for (index1 = 0; index1 < NUM_OF_CARDS_IN_DECK; index1++)
 	{
+		Card temp;
 		//With pointer might be able to make a copy struct functions
 		index2 = randomNumber(0,51);
 		
-		temp.name = g_deck[index1].name;
-		temp.suit = g_deck[index1].suit;
-		temp.value = g_deck[index1].value;
+		strcpy(&temp.name[0], deck.cards[index1].name);
+		temp.suit = deck.cards[index1].suit;
+		temp.value = deck.cards[index1].value;
 		
-		g_deck[index1].name = g_deck[index2].name;
-		g_deck[index1].suit = g_deck[index2].suit;
-		g_deck[index1].value = g_deck[index2].value;
+		strcpy(deck.cards[index1].name, deck.cards[index2].name);
+		deck.cards[index1].suit = deck.cards[index2].suit;
+		deck.cards[index1].value = deck.cards[index2].value;
 
-		g_deck[index2].name = temp.name;
-		g_deck[index2].suit = temp.suit;
-		g_deck[index2].value = temp.value;
+		strcpy(deck.cards[index2].name, temp.name);
+		deck.cards[index2].suit = temp.suit;
+		deck.cards[index2].value = temp.value;
+
 	}
 }
 
@@ -229,7 +291,7 @@ void checkForBlackjack(short whoseCard) //TODO
 		break;
 
 		default:
-			cout << "FATAL ERROR: check for blackjack was passed invalid variable (" << whoseCards << ")";
+			cout << "FATAL ERROR: check for blackjack was passed invalid variable ("whoseCards")";
 		break;
 	}
 	*/
@@ -245,9 +307,8 @@ void actionValidation(void)
 	
 }
 
-void drawCards(int amount, short toWho)  
+void drawCards(int amount, short toWho, Deck &deck, Player &player, Dealer &dealer)  
 {
-	Card cardsDrawn[2]; //2 should be the maximum amount of concurrent card draws.
 
 	if (amount < 0 || amount > 2)
 	{
@@ -259,16 +320,18 @@ void drawCards(int amount, short toWho)
 		case PLAYER:
 			for (int i = 0; i < amount; i++)
 			{
-				g_playersHand[g_playersCardCount].name = g_deck[g_positionInDeck].name;
-				g_playersHand[g_playersCardCount].value = g_deck[g_positionInDeck].value;
-				g_playersHand[g_playersCardCount].suit = g_deck[g_positionInDeck].suit;
+				player.numOfCards++;
+
+				strcpy(player.hand[player.numOfCards].name, deck.cards[deck.posInDeck].name);
+				player.hand[player.numOfCards].value = deck.cards[deck.posInDeck].value;
+				player.hand[player.numOfCards].suit = deck.cards[deck.posInDeck].suit;
 
 				if (i == 0) 
 				{
 					cout << "Player drew ";
 				}
 
-				cout << g_playersHand[g_playersCardCount].name << " of " << g_playersHand[g_playersCardCount].suit;
+				cout << player.hand[player.numOfCards].name << " of " << player.hand[player.numOfCards].suit;
 				if (i < amount - 1)
 				{
 					cout << " and ";
@@ -277,25 +340,26 @@ void drawCards(int amount, short toWho)
 				{
 					cout << endl;
 				}
-
-				g_playersCardCount++;
-				g_positionInDeck++;
+				
+				deck.posInDeck++;
 			}
 		break;
 
 		case DEALER:
 			for (int i = 0; i < amount; i++)
 			{
-				g_dealersHand[g_dealersCardCount].name = g_deck[g_positionInDeck].name;
-				g_dealersHand[g_dealersCardCount].value = g_deck[g_positionInDeck].value;
-				g_dealersHand[g_dealersCardCount].suit = g_deck[g_positionInDeck].suit;
+				dealer.numOfCards++;
+
+				strcpy(dealer.hand[dealer.numOfCards].name, deck.cards[deck.posInDeck].name);
+				dealer.hand[dealer.numOfCards].value = deck.cards[deck.posInDeck].value;
+				dealer.hand[dealer.numOfCards].suit = deck.cards[deck.posInDeck].suit;
 
 				if (i == 0) 
 				{
-					cout << "Dealer drew ";
+					cout << "Dealer drew "; 
 				}
 
-				cout << g_dealersHand[g_dealersCardCount].name << " of " << g_dealersHand[g_dealersCardCount].suit;
+				cout << dealer.hand[dealer.numOfCards].name << " of " << dealer.hand[dealer.numOfCards].suit;
 
 				if (i < amount - 1)
 				{
@@ -306,9 +370,7 @@ void drawCards(int amount, short toWho)
 					cout << endl;
 				}
 
-
-				g_dealersCardCount++;
-				g_positionInDeck++;
+				deck.posInDeck++;
 			}
 		break;
 		
@@ -330,7 +392,7 @@ bool isBustCheck(int cardTotal)
 	}
 }
 
-void mainGameLoop()
+void mainGameLoop(Deck &deck, Player &player, Dealer &dealer)
 {
 	bool gameState = true;
 
@@ -339,27 +401,27 @@ void mainGameLoop()
 		//Betting System
 		cout << "Your Current Balance:" << player.balance << endl;
 		cout << "Please enter your bet: ";
-		cin >> Bet;
+		cin >> player.bet;
 		//HERE
 
-		drawCards(2, PLAYER);
-		drawCards(2, DEALER);
+		drawCards(2, PLAYER, deck, player, dealer);
+		drawCards(2, DEALER, deck, player, dealer);
 		// Screen update function
 
-		if (playersTurn() == BUST)
+		if (playersTurn(deck, player, dealer) == BUST)
 		{
 			cout << "Player busted, Dealer Wins!\n\n";
 		}
 		else
 		{
-			if (dealersTurn() == BUST)
+			if (dealersTurn(deck, player, dealer) == BUST)
 			{
 				cout << "Dealer busted, Player Wins!\n\n";
 			}
 			else
 			{
-				short playersTotal = handTotal(g_playersHand, g_playersCardCount);
-				short dealersTotal = handTotal(g_dealersHand, g_dealersCardCount);
+				short playersTotal = handTotal(player.hand, player.numOfCards);
+				short dealersTotal = handTotal(dealer.hand, dealer.numOfCards);
 
 				if(playersTotal == dealersTotal)
 				{
@@ -381,16 +443,17 @@ void mainGameLoop()
 				}
 			}
 		}
-		resetHands();
-		if (g_positionInDeck > 51) {
+		resetHands(player, dealer);
+		if (deck.posInDeck > (NUM_OF_CARDS_IN_DECK - (MAX_NUM_OF_CARDS_IN_HAND * 2)) - 1) {
 			//Deck Finished
+			// TODO:RESTART GAME SMOOTH
 			gameState = false;
 		}
 		
 	}
 }
 
-short playersTurn()
+short playersTurn(Deck &deck, Player &player, Dealer &dealer)
 {
 	bool turnContinuing = true;
 	int playersCommand = 0;
@@ -398,8 +461,8 @@ short playersTurn()
 	//blackjack check
 	while(turnContinuing == true)
 	{
-		cout << "Dealers Hand Total: " << handTotal(g_dealersHand, g_dealersCardCount) << endl;
-		cout << "Your Hand Total: " << handTotal(g_playersHand, g_playersCardCount) << endl;
+		cout << "Dealers Hand Total: " << dealer.hand[0].value << endl;
+		cout << "Your Hand Total: " << handTotal(player.hand, player.numOfCards) << endl;
 		cout << "Hit or Stand:\n";
 		cin >> playersCommand;
 		// Need input validation/conversion later
@@ -407,8 +470,8 @@ short playersTurn()
 		{
 			case 1:
 				//hit
-				drawCards(1,PLAYER);
-				if(isBustCheck(handTotal(g_playersHand, g_playersCardCount)) == true)
+				drawCards(1,PLAYER, deck, player, dealer);
+				if(isBustCheck(handTotal(player.hand, player.numOfCards)) == true)
 				{
 					//maybe replace with ternary
 					return BUST;
@@ -427,20 +490,20 @@ short playersTurn()
 	return NORMAL;
 }
 
-short dealersTurn() 
+short dealersTurn(Deck &deck, Player &player, Dealer &dealer) 
 {
 	bool turnContinuing = true;
 
 	//blackjack check
 	while(turnContinuing == true)
 	{
-		cout << "Dealers Hand Total: " << handTotal(g_dealersHand, g_dealersCardCount) << endl;
+		cout << "Dealers Hand Total: " << handTotal(dealer.hand, dealer.numOfCards) << endl;
 
-		if(handTotal(g_dealersHand, g_dealersCardCount) < 17)
+		if(handTotal(dealer.hand, dealer.numOfCards) < 17)
 		{
 			//hit
-			drawCards(1,DEALER);
-			if (isBustCheck(handTotal(g_dealersHand, g_dealersCardCount)) == true)
+			drawCards(1,DEALER, deck, player, dealer);
+			if (isBustCheck(handTotal(dealer.hand, dealer.numOfCards)) == true)
 			{
 				return BUST;
 			}
@@ -448,37 +511,14 @@ short dealersTurn()
 		else
 		{
 			//stand
+			cout << "Dealer Stands" << endl;
 			turnContinuing = false;
 		}
+		cout << "Press Enter to Continue..." << endl;
+		cin.get();
+		cin.ignore();
 	}
+
 	return NORMAL;
 }
 
-int main(void) 
-{
-	Player player;
-	
-	resetDeck();
-	resetHands();
-	shuffleDeck();
-	mainGameLoop();
-	
-	/* debug loop
-	for (int i = 0; i < 52; i++)
-	{
-		cout << i << ", " << g_deck[i].name << ", " << g_deck[i].value << ", " << g_deck[i].suit << endl;
-	}
-
-	debug hand display
-	drawCards(2,PLAYER);
-	drawCards(2,DEALER);
-	cout << "Player Hand: "<< g_playersHand[0].name << ", " << g_playersHand[0].value << ", " <<  g_playersHand[0].suit << ";\n" << g_playersHand[1].name << ", " << g_playersHand[1].value << ", " << g_playersHand[1].suit << ";" << endl;
-	cout << "Dealer Hand: "<< g_dealersHand[0].name << ", " << g_dealersHand[0].value << ", " <<  g_dealersHand[0].suit << ";\n" << g_dealersHand[1].name << ", " << g_dealersHand[1].value << ", " << g_dealersHand[1].suit << ";" << endl;
-	*/
-	//Menu loop
-	//Cin plus validation
-	//Switch statement
-	//End Menu loop
-	
-	return 0;
-}
